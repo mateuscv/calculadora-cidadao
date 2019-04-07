8a3054e5a7cf230f8f053fee2aac3d27a9d4cfa0
Mateus Cappellari Vieira <mateusv@email.com>
2019-04-07 18:21:27 -0300

    No refresh; JQuery para os fields, formulas, verif. de erros

--- js/main.js
+++ js/main.js
@@ -1,10 +1,76 @@
+$(document).ready(function () {
+    $('#BotaoID').click(function () {
+        let vp = $("#formID #vp_field").val();
+        let tj = $("#formID #tj_field").val();
+        let periodo = $("#formID #periodo_field").val();
+        let vf = $("#formID #vf_field").val();
+        console.log(vp);
+        console.log(tj);
+        console.log(periodo);
+        console.log(vf);
+        let listaDeValores = [vp,tj,periodo,vf];
+        let cont=0;
+        let vazio="";
+        let result = 0;
+        for (let i=0; i<4; i++){
+            if(listaDeValores[i]===""){
+                cont++;
+                if(i===0){
+                    vazio="vp";
+                }
+                if(i===1){
+                    vazio="tj";
+                }
+                if(i===2){
+                    vazio="periodo";
+                }
+                if(i===3){
+                    vazio="vf";
+                }
+            }
+        }
 
-(function ($) {
+        vp = parseFloat(vp)
+        tj = parseFloat(tj)
+        periodo = parseFloat(periodo)
+        vf = parseFloat(vf)
+
+        if(cont>=2){
+            window.alert(" Deve haver apenas 3 valores.");
+        }
+        else if (cont===0){
+            window.alert(" Você deve inserir 3 dos valores, para calcular o 4º.");
+        }
+        else {
+            switch (vazio) {
+                case"vp":
+                    result = vf / (Math.pow((1 + tj), periodo));
+                    break;
+                case"tj":
+                    result = (Math.pow(vf / vp, 1 / periodo) - 1) * 100;
+                    break;
+                case"periodo":
+                    result = Math.ceil(Math.log(vf / vp) / Math.log(1 + tj));
+                    break;
+                case"vf":
+                    result = vp * Math.pow((1 + tj), periodo);
+                    break;
+                default:
+                    result = 0;
+                    break;
+            }
+        }
+        console.log(result);
+    })
+});
+
+
+/*(function ($) {
     "use strict";
 
     
     /*==================================================================
-    [ Validate ]*/
+    [ Validate ]
     var input = $('.validate-input .input100');
 
     $('.validate-form').on('submit',function(){
@@ -37,7 +103,7 @@
                     return false;
                 }
             }
-        }*/
+        }
     }
 
     function showValidate(input) {
@@ -52,9 +118,6 @@
         $(thisAlert).removeClass('alert-validate');
     }
     
-	function getVals(input) {
-		console.log(input);
-	}
     
 
-})(jQuery);
+})(jQuery)*/
